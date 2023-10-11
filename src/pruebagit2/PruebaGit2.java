6f1e6d79bb3e54fe28999ba3f5f2b798d266ddd4
VictorEstebanH <vicesthol23@gmail.com>
2023-10-11 14:55:29 -0500

    commit1

---
+++ src/pruebagit2/PruebaGit2.java
@@ -0,0 +1,17 @@
+
+package pruebagit2;
+
+
+public class PruebaGit2 {
+
+   
+    public static void main(String[] args) {
+      
+        int n=0;
+        int m=2;
+        int suma = n+m;
+        
+        System.out.println(suma);
+    }
+    
+}
